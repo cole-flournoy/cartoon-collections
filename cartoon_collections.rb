3127fa80ebc39d42eb9716d0deba4ba4ce978580
Cole Flournoy <flournoy.cole@gmail.com>
2020-12-11 15:35:41 -0500

    commit tree 256a66310def64200cf6ed318f4077e24444be84

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,50 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves(array)  
+  array.map.with_index(1) do |dwarf, number|
+    puts "#{number}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"] 
+roll_call_dwarves(dwarves)
+
+def summon_captain_planet (calls)
+  calls.map do |call|
+    call = call.capitalize
+    call << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"] 
+summon_captain_planet(planeteer_calls)
+
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    true if call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+short_words = ["puff", "go", "two"]
+long_planeteer_calls(short_words)
+
+assorted_words = ["two", "go", "industrious", "bop"]
+long_planeteer_calls(assorted_words)
+
+
+def find_the_cheese(foods)
+  cheeses = ["cheddar", "gouda", "camembert"]
+  foods.find do |food|
+    cheeses.include?(food)
+  end
 end
+
+snacks = ["crackers", "gouda", "thyme"]
+find_the_cheese(snacks)
+
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(soup)
+
+ingredients = ["garlic", "rosemary", "bread"]
+find_the_cheese(ingredients)
+
